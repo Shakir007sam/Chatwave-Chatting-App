c38725fac2f58c9a9133112ca722dcb935e4c5d8
Shakir007sam <abdulshakirsam45@example.com>
2024-08-02 15:43:32 +0530

    Updated configuration to use environment variables

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // Add this line at the top to load environment variables
+
 const express = require("express");
 const { createServer } = require("http");
 const path = require("path");
@@ -9,72 +11,77 @@ const MongoStore = require('connect-mongo');
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
-Port = 4000;
+
+// Use environment variables
+const PORT = process.env.PORT || 4000;
+const DB_USER = process.env.DB_USER;
+const DB_PASS = process.env.DB_PASS;
+const DB_HOST = process.env.DB_HOST;
+const DB_NAME = process.env.DB_NAME;
+const DB_OPTIONS = process.env.DB_OPTIONS;
+const SESSION_SECRET = process.env.SESSION_SECRET;
 
 const sessionMiddleware = session({
-    secret: "changeit",
+    secret: SESSION_SECRET,
     resave: false,
     saveUninitialized: true,
-    store: MongoStore.create({ mongoUrl: 'mongodb+srv://abdulshakirsam45:Hello@cluster0.sfiktyh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'})
-  });
+    store: MongoStore.create({
+        mongoUrl: `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?${DB_OPTIONS}`
+    })
+});
 
 app.use(sessionMiddleware);
 io.engine.use(sessionMiddleware);
 
 app.set("view engine", "hbs");
 app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res)=>{
+app.get("/", (req, res) => {
     res.render("home");
-})
+});
 
-app.get("/login", (req, res)=>{
-    if(req.session.isLogin){
+app.get("/login", (req, res) => {
+    if (req.session.isLogin) {
         return res.redirect("/profile");
     }
     res.render("login");
-})
+});
 
-app.post("/login", (req, res)=>{
-    const {name} = req.body;
+app.post("/login", (req, res) => {
+    const { name } = req.body;
     req.session.name = name;
     req.session.isLogin = true;
     res.redirect("/profile");
-})
+});
 
-app.get("/profile", (req, res)=>{
-    if(!req.session.isLogin){
+app.get("/profile", (req, res) => {
+    if (!req.session.isLogin) {
         return res.redirect("/login");
     }
     res.render("profile", {
-        name:req.session.name
+        name: req.session.name
     });
-})
+});
 
-app.get("/logout", (req, res)=>{
-    req.session.destroy((err)=>{
-        if(err) res.send(err);
+app.get("/logout", (req, res) => {
+    req.session.destroy((err) => {
+        if (err) res.send(err);
         res.redirect("/");
-    })
-})
+    });
+});
 
-io.on("connection", (socket)=>{
-
+io.on("connection", (socket) => {
     require("./Socket/newUser")(socket, io);
- 
     require("./Socket/newmessage")(socket, io);
-
     require("./Socket/disconnect")(socket, io);
+});
 
-    
-})
-
-mongoose.connect("mongodb+srv://abdulshakirsam45:Hello@cluster0.sfiktyh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-.then(()=>{
-    httpServer.listen(Port, ()=>{
-        console.log("Server Running at Port", Port);
-    })
-}).catch(()=>{
-    console.log("unable to connect");
-})
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?${DB_OPTIONS}`)
+.then(() => {
+    httpServer.listen(PORT, () => {
+        console.log("Server Running at Port", PORT);
+    });
+}).catch((err) => {
+    console.log("Unable to connect", err);
+});
